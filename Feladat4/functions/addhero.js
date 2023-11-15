8bfc0b9d7d23f83de6fd939b5e68ec5637bf5d10
Kugesz <kovacsgergo.algyo@gmail.com>
2023-11-15 17:23:42 +0100

    valtas gepre hihi

--- Feladat4/functions/addhero.js
+++ Feladat4/functions/addhero.js
@@ -1,3 +1,7 @@
+const image = document.getElementById("newImage");
+
+images = ["ashoka", "boba_fett", "han", "fortnite1", "fortnite2", "fortnite3", "fortnite4"]
+
 function AddHero(newNameInput, newJobInput) {
   let newName = newNameInput.value.split(" ");
   let newJob = newJobInput.value;
@@ -7,6 +11,25 @@ function AddHero(newNameInput, newJobInput) {
   heroes.push({ firstName: newName[0], lastName: newName[0], job: newJob, iconName: "" });
 }
 
-function ChangePicture(){
+function ImageForward() {
+  let currentImage = image.tabIndex(image.src);
+
+  if(currentImage == images.length - 1){
+    currentImage.src = images[0];
+    return
+  }
+
+  currentImage.src = images[currentImage + 1];
   
+}
+
+function ImageBack() {
+  let currentImage = "pics/" + image.tabIndex(image.src) + ".jpg";
+
+  if(currentImage == 0){
+    currentImage == "pics/" + images[images.length - 1] + ".jpg";
+    return
+  }
+
+  currentImage.src = images[currentImage - 1];
 }
